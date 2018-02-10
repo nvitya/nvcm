e5b6c74ee52bd6ea223618ad64ed15c1726f4810
nvitya <nvitya@gmail.com>
2018-02-10 21:42:51 +0100

    License header added

--- mcu/STM32/src/hwpins_stm32.cpp
+++ mcu/STM32/src/hwpins_stm32.cpp
@@ -1,4 +1,30 @@
-// hwpins_stm32.cpp
+/* -----------------------------------------------------------------------------
+ * This file is a part of the NVCM project: https://github.com/nvitya/nvcm
+ * Copyright (c) 2018 Viktor Nagy, nvitya
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software. Permission is granted to anyone to use this
+ * software for any purpose, including commercial applications, and to alter
+ * it and redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software in
+ *    a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ * --------------------------------------------------------------------------- */
+/*
+ *  file:     hwpins_stm32.cpp
+ *  brief:    STM32 Pin/Pad and GPIO configuration
+ *  version:  1.00
+ *  date:     2018-02-10
+ *  authors:  nvitya
+*/
 
 #include "platform.h"
 #include "hwpins.h"
